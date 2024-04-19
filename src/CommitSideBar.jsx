ec4ba764a07f498d0a124c77ff383dd50ba95ea2
Juan Barba <barbajuan11@gmail.com>
2024-04-19 14:49:39 -0700

    Move functionality from ToggleCommits to CommitSideBar

--- src/CommitSideBar.jsx
+++ src/CommitSideBar.jsx
@@ -1,7 +1,34 @@
 import React, {useState, useEffect} from "react";
 
-const CommitSideBar = () => {
+const CommitSideBar = (props) => {
+    const commitLists = () => {
+        let result = props.commits
+            .map((commit) =>
+                <div key={commit.node_id} className="commitInfo grid">
+                    <ul className="repoTitles">
+                        <li>Username</li>
+                        <li>Date</li>
+                        <li>Hash</li>
+                    </ul>
+                    <ul>
+                        <li>: {commit.commit.author.name}</li>
+                        <li>: {commit.commit.author.date}</li>
+                        <li>: {commit.sha}</li>
+                    </ul>
+                    <div className="repoTitles">Message: </div>
+                    <span>{commit.commit.message}</span>
+                </div>
+            )
+        return (
+            <div className="commitList">{result}</div>
+        );
+    };
 
+    return (
+        <div>
+            commitLists()
+        </div>
+    );
 };
 
 export default CommitSideBar;
